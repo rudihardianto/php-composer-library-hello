3777e1b3b1922df3592ef1d8fda02d2f54be48a5
Rudi Hardianto <77372397+rudihardianto@users.noreply.github.com>
2023-12-06 10:22:53 +0700

    add default value

--- src/Customer.php
+++ src/Customer.php
@@ -9,7 +9,7 @@ class Customer
 
     }
 
-    public function sayHello(string $name): string
+    public function sayHello(string $name = "guest"): string
     {
         return "Hello $name, my name is $this->name";
     }
